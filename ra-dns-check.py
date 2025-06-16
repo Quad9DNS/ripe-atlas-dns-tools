875885b38fd6fdc540dc51b206e72eebe2a110b1
Ensar Sarajčić <dev@ensarsarajcic.com>
2025-06-16 14:28:21 +0200

    Add an option to force IPv4 connections

--- ra-dns-check.py
+++ ra-dns-check.py
@@ -219,6 +219,10 @@ options_sample_dict = {
         'default': int(time.time()),
         'help': 'Staleness of the data/records that exceed this  value would be ignored. Staleness is determine between now() and record timestamp',
         'type': 'integer'},
+    'force_ipv4': {
+        'default': False,
+        'help': 'Toggle to disable IPv6 outbound connections and force IPv4',
+        'type': 'boolean'},
 
 }
 
@@ -294,6 +298,7 @@ parser.add_argument('--autocomplete', help=options_sample_dict['autocomplete']['
 parser.add_argument('--probes', help=options_sample_dict['probes']['help'], type=str, default=options_sample_dict['probes']['default'])
 parser.add_argument('--id_servermethod', help=options_sample_dict['id_servermethod']['help'], type=str, choices=valid_id_server_method, default=options_sample_dict['id_servermethod']['default'])
 parser.add_argument('--scrape_staleness_seconds', help=options_sample_dict['scrape_staleness_seconds']['help'], type=int, default=options_sample_dict['scrape_staleness_seconds']['default'])
+parser.add_argument('--force_ipv4', help=options_sample_dict['force_ipv4']['help'], action='store_true', default=options_sample_dict['force_ipv4']['default'])
 parser.add_argument('filename_or_msmid', help='one or two local filenames or RIPE Atlas Measurement IDs', nargs='+')
 parser.format_help()
 argcomplete.autocomplete(parser)
@@ -994,6 +999,12 @@ def check_freshness(timestamp,staleness):
 ######
 #
 # Data loading and summary stats reporting loop ...
+
+
+if args[0].force_ipv4:
+    import requests
+    requests.packages.urllib3.util.connection.HAS_IPV6 = False
+
 
 try:
     probes = args[0].probes.split(',')
