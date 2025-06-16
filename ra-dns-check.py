5d567bc1cb099aa55ce12822fe7ca3262da73dc9
Ensar Sarajčić <dev@ensarsarajcic.com>
2025-06-16 17:50:08 +0200

    Add support for force_ipv6 flag

--- ra-dns-check.py
+++ ra-dns-check.py
@@ -221,7 +221,11 @@ options_sample_dict = {
         'type': 'integer'},
     'force_ipv4': {
         'default': False,
-        'help': 'Toggle to disable IPv6 outbound connections and force IPv4',
+        'help': 'Toggle to force IPv4 outbound connections',
+        'type': 'boolean'},
+    'force_ipv6': {
+        'default': False,
+        'help': 'Toggle to force IPv6 outbound connections',
         'type': 'boolean'},
 
 }
@@ -299,6 +303,7 @@ parser.add_argument('--probes', help=options_sample_dict['probes']['help'], type
 parser.add_argument('--id_servermethod', help=options_sample_dict['id_servermethod']['help'], type=str, choices=valid_id_server_method, default=options_sample_dict['id_servermethod']['default'])
 parser.add_argument('--scrape_staleness_seconds', help=options_sample_dict['scrape_staleness_seconds']['help'], type=int, default=options_sample_dict['scrape_staleness_seconds']['default'])
 parser.add_argument('--force_ipv4', help=options_sample_dict['force_ipv4']['help'], action='store_true', default=options_sample_dict['force_ipv4']['default'])
+parser.add_argument('--force_ipv6', help=options_sample_dict['force_ipv6']['help'], action='store_true', default=options_sample_dict['force_ipv6']['default'])
 parser.add_argument('filename_or_msmid', help='one or two local filenames or RIPE Atlas Measurement IDs', nargs='+')
 parser.format_help()
 argcomplete.autocomplete(parser)
@@ -1004,6 +1009,15 @@ def check_freshness(timestamp,staleness):
 if args[0].force_ipv4:
     import requests
     requests.packages.urllib3.util.connection.HAS_IPV6 = False
+elif args[0].force_ipv6:
+    import requests
+    import socket
+    def ipv6_only():
+        return socket.AF_INET6
+    if requests.packages.urllib3.util.connection.HAS_IPV6:
+        requests.packages.urllib3.util.connection.allowed_gai_family = ipv6_only
+    else:
+        logger.warn("IPv6 not supported. Ignoring --force_ipv6")
 
 
 try:
